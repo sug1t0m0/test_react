fa7ad836d138142a3130cba81849535fda165b3b
sug1t0m0 <t11i281a@gmail.com>
2019-02-15 20:26:41 +0900

    test
    test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
       <SimpleSlider />
     </div>
   );
