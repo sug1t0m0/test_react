c204913c8bfd67b7ddb2e4275161c123f950ff7e
sug1t0m0 <t11i281a@gmail.com>
2019-02-27 16:34:35 +0900

    scssはやめよう

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { SimpleSlider } from "./simpleSlider";
 
-import "./styles.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import "./styles.css";
 
 function App() {
   return (
