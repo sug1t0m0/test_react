93b5dd0747531d1aeb5dbda8a7075a2967ad057e
sug1t0m0 <t11i281a@gmail.com>
2019-02-27 15:12:18 +0900

    arrawsの内容を変更してみる

--- src/simpleSlider.jsx
+++ src/simpleSlider.jsx
@@ -3,6 +3,28 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
+
 export class SimpleSlider extends React.Component {
   render() {
     var settings = {
