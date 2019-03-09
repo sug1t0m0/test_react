d936b56160d731ff15ffb4e6bf5ef024791fc32f
sug1t0m0 <t11i281a@gmail.com>
2019-03-09 21:35:15 +0900

    react-router-domでルーティング

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { SampleSlider } from "./SampleSlider";
 import { ImageSlider } from "./ImageSlider/index.jsx";
-import { Text } from "./atoms/Text/";
-import { Title } from "./atoms/Title/";
+import { NavigationBar } from "./atoms/NavigationBar";
 import { ParagraphWithCenteredElements } from "./molecules/ParagraphWithCenteredElements/";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "slick-carousel/slick/slick.css";
 
@@ -12,19 +12,56 @@ import "./styles.css";
 
 function App() {
   const slideContents = Array.from(Array(6).keys());
+  const slugs = [
+    "ParagraphWithCenteredElements",
+    "ImageSlider",
+    "SampleSlider"
+  ];
   return (
     <div className="App">
-      <ImageSlider slideContents={slideContents} />
-      <SampleSlider slideContents={slideContents} />
-      <ParagraphWithCenteredElements
-        title={"Title"}
-        subTitle={"SubTitle"}
-        body={
-          "\n<p>\u3000\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3002<br></p>\n\n\n\n<p>\u3000\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3002</p>\n\n\n\n<p>\u3000\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3002</p>\n"
-        }
-        innerWidth={"80%"}
-        gapHeight={10}
-      />
+      <Router>
+        <div>
+          <NavigationBar slugs={slugs} />
+          <hr />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <ParagraphWithCenteredElements
+                title={"Title"}
+                subTitle={"SubTitle"}
+                body={
+                  "\n<p>\u3000\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3002<br></p>\n\n\n\n<p>\u3000\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3002</p>\n\n\n\n<p>\u3000\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3002</p>\n"
+                }
+                innerWidth={"80%"}
+                gapHeight={10}
+              />
+            )}
+          />
+          <Route
+            path="/ParagraphWithCenteredElements"
+            render={() => (
+              <ParagraphWithCenteredElements
+                title={"Title"}
+                subTitle={"SubTitle"}
+                body={
+                  "\n<p>\u3000\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3042\u3002<br></p>\n\n\n\n<p>\u3000\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3044\u3002</p>\n\n\n\n<p>\u3000\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3046\u3002</p>\n"
+                }
+                innerWidth={"80%"}
+                gapHeight={10}
+              />
+            )}
+          />
+          <Route
+            path="/ImageSlider"
+            render={() => <ImageSlider slideContents={slideContents} />}
+          />
+          <Route
+            path="/SampleSlider"
+            render={() => <SampleSlider slideContents={slideContents} />}
+          />
+        </div>
+      </Router>
     </div>
   );
 }
